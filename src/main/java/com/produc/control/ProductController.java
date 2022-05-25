b3769f41a48b241ba7bf9a708c47ce5c09dcd679
Bahodir <yahda.seven@gmail.com>
2022-05-25 10:27:11 +0500

    otdi

--- src/main/java/com/produc/control/ProductController.java
+++ src/main/java/com/produc/control/ProductController.java
@@ -19,6 +19,8 @@ public class ProductController {
 
 // comenentariya
 //    olindi
+
+//    dfdfdf
     @GetMapping("/product")
     public ResponseEntity getAll() {
         return ResponseEntity.ok(service.findAll());
